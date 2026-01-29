9b1350a216e3049d354417ea777846fbff1fe43e
Sourav Suman <123897785+souravsumanwgs@users.noreply.github.com>
2026-01-29 15:07:04 +0530

    multiple

--- Test.java
+++ Test.java
@@ -4,8 +4,8 @@ public class Test {
 
     int a=50;
     int b=30;
-    int result = a+b;
-    System.out.println("The sum is " +result);
+    int result = a*b;
+    System.out.println("The multiple is " +result);
     
   }
   public static void main(String[] args)
